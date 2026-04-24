a74c7f5c3e5b1e88a69a6551e179f300377b112c
plaggix <plaggixfalcao@gmail.com>
2026-04-24 15:39:32 +0200

    Correction : Correction de la base de données

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const adminRoutes = require('./routes/admin');
 
 const app = express();
 const prisma = new PrismaClient({
-  datasources: {
+  datasource: {
     db: {
       url: process.env.DATABASE_URL,
     },
