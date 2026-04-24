6899659d243d619c945abd637e9f9f2ab69c61be
plaggix <plaggixfalcao@gmail.com>
2026-04-24 18:06:07 +0200

    Dernier essai : Forcer le constructeur

--- src/index.js
+++ src/index.js
@@ -10,13 +10,7 @@ const projectsRoutes = require('./routes/projects');
 const adminRoutes = require('./routes/admin');
 
 const app = express();
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
+const prisma = new PrismaClient();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
